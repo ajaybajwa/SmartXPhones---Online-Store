7e682f8308610c675c6edfaa3256fc6fc221dff3
ajaybajwa <ajaydeep1717@gmail.com>
2024-10-11 16:28:55 -0400

    update smartxphones logo URL at frontend

--- smartxphones-frontend/src/app/login/login.component.ts
+++ smartxphones-frontend/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
   oktaSignin: any;
   constructor(private oktaAuthService:OktaAuthService) {
     this.oktaSignin = new OktaSignIn({
-      logo: 'https://res.cloudinary.com/ajaybajwa/image/upload/v1724723419/smartxphones/wpx8efjh1q1ysoohw5ex.png',
+      logo: 'https://res.cloudinary.com/ajaybajwa/image/upload/t_logo-resized/v1724723419/Ecommerce/wpx8efjh1q1ysoohw5ex.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,
